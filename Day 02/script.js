1fdcb88cbe54f5665fbdb9f1b9209ffd49d22764
Vivek Mane <vivekmane172@gmail.com>
2025-09-10 21:21:30 +0530

    Functions

---
+++ Day 02/script.js
@@ -0,0 +1,17 @@
+// How functions work in JavaScript and working of call stack
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a()
+{
+    var x = 10;
+    console.log(x);
+}
+function b()
+{
+    var x = 100;
+    console.log(x);
+}
+
