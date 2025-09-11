86db73c95be2caae53648af346a6e6fcef44f232
Vivek Mane <vivekmane172@gmail.com>
2025-09-11 17:42:17 +0530

    Undefinded & not defined

---
+++ Day 04/script.js
@@ -0,0 +1,20 @@
+var a;
+console.log(a); // undefined
+//undefined is when the value is not yet assinged to the variable
+a = 10;
+console.log(a); // 10
+console.log(x); //error : not defined
+// Not defined error occurs when the variable is never declared in the code
+
+
+
+// Loosely typed
+var n;
+console.log(n); // undefined
+n = 10;
+console.log(n); // 10 - integer
+n = "hello"
+console.log(n); // hello - string
+n = 9.99; 
+console.log(n); // 9.99 - float
+
