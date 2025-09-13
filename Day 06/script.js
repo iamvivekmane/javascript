14ed80ea2f236aef0b5327426bf73523bfcfc340
Vivek Mane <vivekmane172@gmail.com>
2025-09-13 21:30:58 +0530

    let, const & temporal dead zone

--- Day 06/script.js
+++ Day 06/script.js
@@ -1,1 +1,31 @@
-console.log("Hello");
+// console.log(a); // ReferenceError: Cannot access 'a' before initialization
+
+// console.log(x); //  ReferenceError: x is not defined
+
+
+let a = 10;
+
+var b = 100;
+console.log(b); // 100
+
+
+
+// let a = 100; // SyntaxError: Identifier 'a' has already been declared
+
+
+let y;
+y = 10; // allowed
+
+// const z;
+// z =100; //not allowed
+
+//typeError
+// const n = 10;
+// n=1000;
+
+//syntaxError
+// const k;
+
+//referenceError
+// console.log(r);
+
