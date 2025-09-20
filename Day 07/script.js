9d5bbbcf7cd6be39dd3dd8c7589c1029a8a66f37
Vivek Mane <vivekmane172@gmail.com>
2025-09-20 21:31:25 +0530

    Block scope & Shadowing

--- Day 07/script.js
+++ Day 07/script.js
@@ -1,1 +1,32 @@
-console.log("Hello");
+// //{ } indicates the block
+// if(true)
+// {
+//     //Compound statement
+//     let a = 10;
+//     console.log(a);
+// }
+
+
+// {
+//     var a = 10;
+//     let b = 20;
+//     const c = 30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(a);
+// console.log(b); // ReferenceError: b is not defined
+// console.log(c); // ReferenceError: c is not defined
+
+// works same with lexical chain
+const a = 10;
+{
+    const a = 100;
+    {
+        const a = 200;
+        console.log(a); // 200
+    }
+    console.log(a); // 100
+}
+console.log(a); // 10
