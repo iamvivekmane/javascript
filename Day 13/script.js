93d119deee726590022df1caab7a7cf8f8102237
Vivek Mane <vivekmane172@gmail.com>
2025-10-06 20:56:59 +0530

    Asynchronous JavaScript & Event loop

--- Day 13/script.js
+++ Day 13/script.js
@@ -1,1 +1,15 @@
-console.log("Js engine");
+console.log("start");
+setTimeout(() => {
+    console.log("CB statement");
+}, 5000);
+
+fetch("https://jsonplaceholder.typicode.com/todos/1").then(function cBF(){
+    console.log("CB netflix");
+});
+console.log("end");
+
+// output:
+// start
+// end
+// CB netflix
+// CB statement
