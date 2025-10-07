1888c39c6b02cb5230be34164d0df4b2a4c253cc
Vivek Mane <vivekmane172@gmail.com>
2025-10-07 21:24:23 +0530

    More about setTimeOut function

--- Day 15/script.js
+++ Day 15/script.js
@@ -1,1 +1,21 @@
-console.log("Js engine");
+console.log("start");
+setTimeout(function cb(){
+console.log("Callback");
+},5000);
+
+console.log("end");
+
+let startDate = new Date().getTime();
+let endDate = startDate;
+while(endDate <= startDate + 10000){
+    endDate = new Date().getTime();
+}
+console.log("Terminated compilation");
+
+// output:
+// start
+// end
+// Terminated compilation
+// Callback
+
+// The output will be the same even if the delay is 0 in the setTimeOut()
