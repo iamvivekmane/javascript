28dd10659e30c5a8d4cc4c67162bef961bb95119
Vivek Mane <vivekmane172@gmail.com>
2025-10-09 20:56:16 +0530

    Map

---
+++ Day 17/script.js
@@ -0,0 +1,30 @@
+// Map
+const arr = [11,20,30,40];
+
+// function double(x)
+// {
+//     return x*2;
+// }
+// const output = arr.map(double);
+
+// const output = arr.map((x)=>x.toString(2));
+// console.log(output);
+
+
+
+//Filter
+
+// function isOdd(x)
+// {
+//     return x%2;
+// }
+// const output = arr.filter(isOdd);
+
+// const output = arr.filter((x)=>x>20)
+// console.log(output);
+
+
+// Reduce
+
+
+ 
