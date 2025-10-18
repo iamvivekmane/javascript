6f94ec3c6711b52e2ba29ea1bed402183d45202e
Vivek Mane <vivekmane172@gmail.com>
2025-10-18 22:26:54 +0530

    Completed chaining

--- Day 20/script.js
+++ Day 20/script.js
@@ -1,8 +1,31 @@
-let myPromise = new Promise(function(myResolve, myReject) {
-  myResolve(); 
-  myReject();  
-});
-myPromise.then(
-  function(value) {  },
-  function(error) { /* code if some error */ }
-);
+class Calculator {
+  constructor(initialValue = 0) {
+    this.value = initialValue;
+  }
+
+  add(num) {
+    this.value += num;
+    return this; // Crucial for chaining
+  }
+
+  subtract(num) {
+    this.value -= num;
+    return this; // Crucial for chaining
+  }
+
+  multiply(num) {
+    this.value *= num;
+    return this; // Crucial for chaining
+  }
+
+  getResult() {
+    return this.value;
+  }
+}
+
+const calc = new Calculator(10);
+
+// Chaining methods
+const finalResult = calc.add(5).subtract(2).multiply(3).getResult();
+
+console.log(finalResult); // Output: 39 ( (10 + 5 - 2) * 3 )
